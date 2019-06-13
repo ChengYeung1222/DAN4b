a963a23bfc620078421b290395bc22ce36051fc9
ChengY <chengyann1222@gmail.com>
2019-06-13 16:47:26 +0800

    Using loops to calculate the gaussian kernel

--- mmd.py
+++ mmd.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import torch
+from torch.autograd import Variable
 
 
 # Consider linear time MMD with a linear kernel:
@@ -20,10 +21,65 @@ def mmd_linear(f_of_X, f_of_Y):
 def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
     n_samples = int(source.size()[0]) + int(target.size()[0])
     total = torch.cat([source, target], dim=0)
+    # in resnet: [64,64,2048]
     total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)),
-                                       int(total.size(1)))  # torch.Size([64, 64, 2048])
+                                       int(total.size(1)))
     total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
     L2_distance = ((total0 - total1) ** 2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples ** 2 - n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul ** i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)  # /len(kernel_val)
+
+
+def guassian_kernel_two_loops(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0]) + int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    L2_distance=torch.zeros(int(total.size(0)), int(total.size(0))).cuda()
+    L2_distance = Variable(L2_distance)
+    for i in range(total.size()[0]):
+        for j in range(total.size()[0]):
+            square = (total[i] - total[j]) ** 2
+            L2_distance[i, j] = square.sum()
+
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples ** 2 - n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul ** i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)  # /len(kernel_val)
+
+def guassian_kernel_one_loop(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0]) + int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    L2_distance=torch.zeros(int(total.size(0)), int(total.size(0))).cuda()
+    L2_distance = Variable(L2_distance)
+    for i in range(total.size()[0]):
+        L2_distance[i,:]=torch.sum((total[i,:]-total)**2,1)
+
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples ** 2 - n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul ** i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)  # /len(kernel_val)
+
+def guassian_kernel_no_loop(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0]) + int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    L2_distance=torch.zeros(int(total.size(0)), int(total.size(0))).cuda()
+    L2_distance = Variable(L2_distance)
+    for i in range(total.size()[0]):
+        L2_distance[i,:]=torch.sum((total[i,:]-total)**2,1)
+
     if fix_sigma:
         bandwidth = fix_sigma
     else:
@@ -49,8 +105,8 @@ def mmd_rbf_accelerate(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=N
 
 def mmd_rbf_noaccelerate(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
     batch_size = int(source.size()[0])  # [32,2048]
-    kernels = guassian_kernel(source, target,
-                              kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    kernels = guassian_kernel_one_loop(source, target,
+                                           kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
     XX = kernels[:batch_size, :batch_size]
     YY = kernels[batch_size:, batch_size:]
     XY = kernels[:batch_size, batch_size:]
