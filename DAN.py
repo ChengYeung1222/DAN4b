61b6e62788ca85da96bc33f5530098cb14478f05
ChengYeung1222 <a371503996@qq.com>
2019-07-27 10:51:48 +0800

    with rebalancing

--- DAN.py
+++ DAN.py
@@ -54,8 +54,8 @@ momentum = 0.9
 no_cuda = False
 seed = 8
 log_interval = 50
-log_interval_test = 10
-l2_decay = 5e-4  # todo:1e-4 5e-4
+log_interval_test = 20
+l2_decay = 1e-3  # todo:5e-4
 root_path = "./"
 source_list = "./train_list.csv"
 target_list = "./pre_list.csv"
@@ -133,8 +133,6 @@ def load_pretrain_alex(model, alexnet_model=True):
                 if not "features.0" in k:
                     if not "cls" in k:
                         model_dict[k] = pretrained_dict[k]
-    # else:
-    #     model.load_state_dict(torch.load(ckpt_model))
     return model
 
 
@@ -152,13 +150,6 @@ def load_pretrain(model, resnet_model=True):
                             if not 'sharedNet.bn1.running_mean' in k:
                                 if not 'sharedNet.bn1.running_mean' in k:
                                     model_dict[k] = pretrained_dict[k[k.find(".") + 1:]]
-    # else:#todo:load ckpt
-    #     model.load_state_dict(torch.load(ckpt_model))
-    #     model_dict = model.state_dict()
-    #     for k, v in model_dict.items():
-    #         if not "cls_fc" in k:
-    #             model_dict[k] = pretrained_dict[k[k.find(".") + 1:]]
-
     return model
 
 
@@ -184,11 +175,12 @@ def load_pretrain_v0(model):
 def train(epoch, model):
     LEARNING_RATE = lr / math.pow((1 + 10 * (epoch - 1) / epochs), 0.75)
     print('learning rate{: .6f}'.format(LEARNING_RATE))
+    # ResNet optimizer
     # optimizer = torch.optim.Adam([
     #     {'params': model.sharedNet.parameters()},  # lr=LEARNING_RATE / 10
     #     {'params': model.cls_fc.parameters(), 'lr': LEARNING_RATE},
     # ], lr=LEARNING_RATE / 10, weight_decay=l2_decay)
-    ## AlexNet optimizer
+    # AlexNet optimizer
     optimizer = torch.optim.Adam([
         # {'params': model.conv1.parameters(), 'lr': LEARNING_RATE},
         {'params': model.features.parameters()},  # lr=LEARNING_RATE / 10
@@ -199,7 +191,7 @@ def train(epoch, model):
         {'params': model.cls4.parameters(), 'lr': LEARNING_RATE},
         {'params': model.l8.parameters(), 'lr': LEARNING_RATE},
         {'params': model.cls_fc.parameters(), 'lr': LEARNING_RATE},
-    ], lr=LEARNING_RATE / 10, weight_decay=l2_decay)#todo:momentum=momentum,
+    ], lr=LEARNING_RATE / 10, weight_decay=l2_decay)  # todo:momentum=momentum,
 
     model.train()
 
@@ -218,7 +210,6 @@ def train(epoch, model):
         # if i % len_source_loader == 0:
         #     iter_source = iter(source_loader)
         if i % len_target_loader == 0:
-            # if True:
             logging.debug('iter_target = iter(target_train_loader)')
             iter_target = iter(target_train_loader)
 
@@ -240,31 +231,17 @@ def train(epoch, model):
         score_source_pred, loss_mmd = model(data_source, data_target)
         logging.debug('Calculating loss_cls...')
         prob_source_pred = F.softmax(score_source_pred, dim=1)
-        # log_softmax_source_pred = F.log_softmax(score_source_pred, dim=1)
         y_1 = prob_source_pred[:, 1]
         y_0 = prob_source_pred[:, 0]
-        # max_prob = torch.max(prob_source_pred, dim=1)[0]
-        # max_index = torch.max(prob_source_pred, dim=1)[1]
-        # min_index = torch.min(prob_source_pred, dim=1)[1]
-        ratio_source = y_1 / y_0 * len_source_zeros / len_source_ones  # todo:
-        # prob_source_pred[:, max_index] = prob_source_pred[:, max_index] * len_source_zeros / len_source_dataset
-        # prob_source_pred[:, min_index] = prob_source_pred[:, min_index] * len_source_ones / len_source_dataset
-        # prob_source_pred[:, max_index] = ratio_source / (ratio_source + 1)
-        # prob_source_pred[:, min_index] = 1 / (ratio_source + 1)
+        ratio_source = y_1 / y_0 * len_source_zeros / len_source_ones
         prob_source_pred_new = torch.Tensor(prob_source_pred.size()).cuda()
         prob_source_pred_new[:, 1] = ratio_source.data / (ratio_source.data + 1)
         prob_source_pred_new[:, 0] = 1 / (ratio_source.data + 1)
         prob_source_pred_new = Variable(prob_source_pred_new)
 
-        # for ii, ind in enumerate(max_index):
-        #     logging.debug('ind=%s' % (ind))
-        #     logging.debug('ind.data=%s' % (ind.cpu().data.numpy()[0]))
-        #     prob_source_pred[ii, 1] = ratio_source[ii] / (ratio_source[ii] + 1)
-        #     prob_source_pred[ii, 0] = 1 / (ratio_source[ii] + 1)
-
-        loss_cls = F.nll_loss(F.log_softmax(score_source_pred,dim=1),
+        loss_cls = F.nll_loss(F.log_softmax(score_source_pred, dim=1),
                               target=label_source)  # the negative log likelihood loss
-        gamma = 2 / (1 + math.exp(-10 * (epoch) / epochs)) - 1  # lambda in DAN paper#todo:-50
+        gamma = 2 / (1 + math.exp(-50 * (epoch) / epochs)) - 1  # lambda in DAN paper#todo:-10
         logging.debug('Push mmd to gpu...')
         # !!!!
         loss_mmd = loss_mmd.cuda()
@@ -355,22 +332,15 @@ def test(epoch, model):
         data, label = Variable(data, volatile=True), Variable(label)
         s_output, _ = model(data, data)
         prob_val_pred = F.softmax(s_output, dim=1)
-        # log_softmax_val_pred = F.log_softmax(s_output, dim=1)
         y_1 = prob_val_pred[:, 1]
         y_0 = prob_val_pred[:, 0]
-        # max_prob = torch.max(prob_val_pred, dim=1)[0]
-        # max_index = torch.max(prob_val_pred, dim=1)[1]
-        # min_index = torch.min(prob_val_pred, dim=1)[1]
         ratio_val = y_1 / y_0 * len_val_zeros / len_val_ones
         prob_val_pred_new = torch.Tensor(prob_val_pred.size()).cuda()
         prob_val_pred_new[:, 1] = ratio_val.data / (ratio_val.data + 1)
         prob_val_pred_new[:, 0] = 1 / (ratio_val.data + 1)
-        # for ii, ind in enumerate(max_index):
-        #     prob_val_pred[ii, ind.cpu().data.numpy()[0]] = ratio_val[ii] / (ratio_val[ii] + 1)
-        #     prob_val_pred[ii, 1 - ind.cpu().data.numpy()[0]] = 1 * (ratio_val[ii] + 1)
         prob_val_pred_new = Variable(prob_val_pred_new)
-        test_loss += F.nll_loss(F.log_softmax(s_output,dim=1), label, size_average=False).data[0]  # sum up batch loss
-        loss_val = F.nll_loss(F.log_softmax(s_output,dim=1), label).data[0]
+        test_loss += F.nll_loss(F.log_softmax(s_output, dim=1), label, size_average=False).data[0]  # sum up batch loss
+        loss_val = F.nll_loss(F.log_softmax(s_output, dim=1), label).data[0]
         pred = prob_val_pred_new.data.max(1)[
             1]  # get the index of the max log-probability, s_output_shape: torch.Size([32, 31])
         correct += pred.eq(label.data.view_as(pred)).cpu().sum()
@@ -392,7 +362,7 @@ def test(epoch, model):
             F1score = 2 * r * p / (r + p)
         else:
             F1score = 0
-        if i % log_interval == 0:
+        if i % log_interval_test == 0:
             vis.line(X=np.array([i + (epoch - 1) * len_test_loader]), Y=np.array([F1score]),
                      win='testing F1 score',
                      update='append',
@@ -415,7 +385,7 @@ def test(epoch, model):
 
 
 if __name__ == '__main__':
-    # model = models.DANNet(num_classes=2)  # Models.py#todo:
+    # model = models.DANNet(num_classes=2)  # Models.py#todo:ResNet
     model = models.DAN_with_Alex(num_classes=2)
     correct = 0
     print(model)
@@ -432,7 +402,5 @@ if __name__ == '__main__':
         if t_correct > correct:
             correct = t_correct
         current_acc = 100. * t_correct / len_test_dataset
-        # vis.line(X=np.array([epoch]), Y=np.array([current_acc]), win='accuracy', update='append',
-        #          opts={'title': 'current accuracy'})
         print('{} source: {} to target: {} max correct: {} max accuracy{: .2f}%\n'.format(
             datetime.now(), source_name, target_name, correct, 100. * correct / len_test_dataset))
